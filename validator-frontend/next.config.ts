00004ad3d838ddef72b87e325061f8e8d2cba947
Zihao Xu <xzhseh@gmail.com>
2024-12-03 21:39:04 -0500

    update next.config.ts for nginx's need

--- validator-frontend/next.config.ts
+++ validator-frontend/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    // enable static HTML export
+    output: 'export',
+    distDir: '.next',
+    images: {
+        // required for static export
+        unoptimized: true,
+    },
+    // add trailing slashes to URLs
+    trailingSlash: true,
+    // configure base path if not serving from domain root
+    // basePath: '',
 };
 
 export default nextConfig;
