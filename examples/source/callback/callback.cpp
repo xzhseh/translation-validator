7be08732d7569a2e18e8b8644778fb90d377fb4a
Zihao Xu <xzhseh@gmail.com>
2024-12-03 20:13:27 -0500

    update the comments for callback

--- examples/source/callback/callback.cpp
+++ examples/source/callback/callback.cpp
@@ -1,3 +1,5 @@
+/// function pointer example, the verification will timeout.
+/// note: function pointer is one of the unsupported features of alive2.
 int process(int (*callback)(int), int x) {
     return callback(x);
 }
--- examples/source/callback/callback.rs
+++ examples/source/callback/callback.rs
@@ -1,3 +1,6 @@
+/// it is worth noting that this translation follows the simplest
+/// function pointer style in rust, in order to type check
+/// with the cpp llvm ir when verifying by alive2.
 pub fn process(callback: fn(i32) -> i32, x: i32) -> i32 {
     callback(x)
 }
