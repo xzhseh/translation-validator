bd6b358b5780c806cba8772ed64a9ffb523da224
Zihao Xu <xzhseh@gmail.com>
2024-12-09 23:32:36 -0500

    update README.md for translation validator

--- examples/source/deref/deref.rs
+++ examples/source/deref/deref.rs
@@ -1,4 +1,4 @@
-/// a safer translation of the original `deref.cpp`
+/// a "safer" translation of the original `deref.cpp` - is this semantically equivalent?
 pub fn deref(ptr: &i32) -> i32 {
     // `ptr` is guaranteed non-null by type system,
     // so that this is always safe, reference is guaranteed valid.
